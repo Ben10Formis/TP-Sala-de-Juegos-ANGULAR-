994f4babb2a83274a588e0503c7ae7dc4a3a20da
Ben10Formis <ibarraromyna@gmail.com>
2019-09-12 01:55:02 -0300

    Más modficiaciones
    Favicon

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+//https://material.angular.io/guide/getting-started
+import 'hammerjs';
+
 if (environment.production) {
   enableProdMode();
 }
