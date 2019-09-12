a2eea615fa6523ea08a83ed83969df9d1bfbad11
Ben10Formis <ibarraromyna@gmail.com>
2019-09-12 01:39:48 -0300

    Agrego mi juego
    Primera carga

--- src/app/clases/juego.ts
+++ src/app/clases/juego.ts
@@ -1,3 +1,39 @@
+/*export abstract class Juego {
+  public nombre = 'Sin Nombre';
+  public jugador: string;
+  public gano = false;
+  public user = JSON.parse(localStorage.getItem("usuario"));
+  
+  constructor(nombre?: string, gano?: boolean,jugador?:string) {
+  /*  if (nombre)
+      this.nombre = nombre;
+    if (gano)
+      this.gano = gano;
+    if(jugador)
+      this.jugador=jugador;
+    else
+      this.jugador= "natalia natalia";*/
+
+/*    if (nombre)
+      this.nombre = nombre;
+    if(this.user == null){
+     this.jugador = "Invitado@admin.com";
+    }
+    else{
+      this.jugador=this.user.email;
+    }
+    if (gano)
+    this.gano = gano;
+  }
+
+  public abstract verificar():boolean; 
+  
+  public retornarAyuda() {
+    
+    return "NO hay Ayuda definida";
+  }
+}
+*/
 export abstract class Juego {
   public nombre = 'Sin Nombre';
   public jugador: string;
@@ -12,7 +48,7 @@ export abstract class Juego {
     if(jugador)
       this.jugador=jugador;
     else
-      this.jugador= "natalia natalia";
+      this.jugador= "testeo usuario";
   }
 
 
